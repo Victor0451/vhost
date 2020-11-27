d25ccc625e143abee38e8a482afe037d9f11f7b3
victor0451 <victor.m.longo@gmail.com>
2020-11-27 09:59:38 -0300

    Ejemplo y prueba de servidor de virtualizacion

---
+++ server.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
+const express = require("express");
+const serveIndex = require("serve-index");
+const app = express();
+const morgan = require("morgan");
+
+const privateKey = fs.readFileSync("certs/server.key", "utf8");
+const certificate = fs.readFileSync("certs/server.cert", "utf8");
+
+const vhost = (hostname, app) => (req, res, next) => {
+  const host = req.headers.host.split(":")[0];
+
+  hostname = new RegExp(hostname);
+
+  if (hostname.test(host)) {
+    return app(req, res, next);
+  } else {
+    next();
+  }
+};
+
+app.use(morgan("dev"));
+
+// Configure your vhosts here
+app.use(
+  vhost(
+    "sepelios.werchow.com",
+    express()
+      .get("/", (req, res) =>
+        res.sendFile(__dirname + "/projects/example/index.html")
+      )
+      .get("/test", (req, res) => res.send("example test page"))
+      .use("/images", express.static("projects/example/images")) // example of static file
+  )
+);
+
+// localhost with directory listing
+app.use("/", express.static("projects"), serveIndex("projects"));
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(
+  { key: privateKey, cert: certificate },
+  app
+);
+
+httpServer.listen(80, () => console.log("Server listening on port 80."));
+httpsServer.listen(443, () => console.log("Server listening on port 443."));
